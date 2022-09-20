f7fca1284b0a673a0330d7effd6b4ccbf5b2dfe5
JohnsonVasanth <113984262+JohnsonVasanth@users.noreply.github.com>
2022-09-20 16:44:13 +0530

    Add files via upload

---
+++ DailySharePrice/DailySharePrice/Controllers/DailySharePriceController.cs
@@ -0,0 +1,53 @@
+﻿using DailySharePrice.Models;
+using DailySharePrice.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailySharePrice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DailySharePriceController : ControllerBase
+    {
+        private readonly ISharePriceRepository sharePriceRepository;
+        public DailySharePriceController(ISharePriceRepository _sharePriceRepository)
+        {
+            sharePriceRepository = _sharePriceRepository;
+        }
+        [HttpGet]
+        [Route("stockname")]
+        public IActionResult GetStockByName(string stockname)
+        {
+            
+                if (stockname == null)
+                {
+                    return BadRequest();
+                }
+                var shareData = sharePriceRepository.GetShareByNameRepository(stockname);
+                if (shareData == null)
+                {
+                    return BadRequest();
+                }
+                return Ok(shareData);
+
+            
+        }
+        [HttpGet]
+        public ActionResult GetAllStocks()
+        {
+            var l = sharePriceRepository.GetAllstocks();
+            return Ok(l);
+        
+
+        }
+
+
+
+
+
+    }
+}
---
+++ DailySharePrice/DailySharePrice/Repository/SharePriceRepository.cs
@@ -0,0 +1,29 @@
+﻿using DailySharePrice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailySharePrice.Repository
+{
+    public class SharePriceRepository:ISharePriceRepository
+    {
+        private readonly SharePriceContext db;
+        public SharePriceRepository(SharePriceContext _db)
+        {
+            db = _db;
+        }
+        public DailyStockDetails GetShareByNameRepository(string shareName)
+        {
+            DailyStockDetails shareData = null;
+            
+            shareData= db.DailyStockDetails.Where(x => x.StockName == shareName).FirstOrDefault();
+            return shareData;
+        }
+        public List <DailyStockDetails> GetAllstocks()
+        {
+            return db.DailyStockDetails.ToList();
+        }
+        
+    }
+}
